9f84d134698dfb0513ae7b80665a27f0424e9271
marlamoury <marlamoury@hotmail.com>
2025-01-18 14:19:49 -0300

    ConnectionString

--- ContactManagement.Api/Configurations/DependencyInjectionConfig.cs
+++ ContactManagement.Api/Configurations/DependencyInjectionConfig.cs
@@ -10,7 +10,7 @@ public static class DependencyInjectionConfig
 {
     public static IServiceCollection ResolveDependencies(this IServiceCollection services, IConfiguration configuration)
     {
-        string connectionString = configuration.GetConnectionString("ConnectionLucas");
+        string connectionString = configuration.GetConnectionString("ConnectionMarla");
 
         services.AddTransient<IDbConnection>(db => new SqlConnection(connectionString));
         services.AddScoped<IContactServices, ContactServices>();
